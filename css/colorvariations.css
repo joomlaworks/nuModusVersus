52785e004a46f3480ca4487e3e072a1e9e795468
Yiota Ziaggou <yiota@nuevvo.com>
2013-07-26 19:20:33 +0300

    updated with the correct file versioning (v1.0.1)

--- css/colorvariations.css
+++ css/colorvariations.css
@@ -1,5 +1,5 @@
 /**
- * @version     1.0.0
+ * @version     1.0.1
  * @package     nuModusVersus
  * @author      Nuevvo - http://nuevvo.com
  * @copyright   Copyright (c) 2010 - 2013 Nuevvo Webware Ltd. All rights reserved.
